d09227b2670ac911298cc1a2041a1059afe212b8
zhang <1005941448@qq.com>
2020-12-30 16:53:46 +0800

    修改I2C地址0X2f

--- main.ts
+++ main.ts
@@ -7,7 +7,7 @@ load dependency
 
 //% color="#006400" weight=20 icon="\uf0a1"
 namespace Speech {
-    const I2C_ADDR = 0x50                   //模块地址
+    const I2C_ADDR = 0x2f                   //模块地址
     const DATA_HEAD = 0xFD                  //帧头
     
 
